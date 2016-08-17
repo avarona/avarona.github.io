7ea1ce1599dd7ab93428f9da2cb3f6469ed2ba29
avarona <avarona10@gmail.com>
2016-08-17 01:20:48 -0400

    added projects & images/links

--- js/index.js
+++ js/index.js
@@ -1,16 +1,14 @@
-$(document).ready(function() {
-  $('#projects img').hover(
-    function() {
-      //$(this).animate({opacity: .5}, '1000');
-      $(this).addClass('jq-thumb', 'fast');
-    },
-    function() {
-      //$(this).animate({opacity: 1}, 'fast');
-      $(this).removeClass('jq-thumb', 'fast');
+var main = function() {
+  $("form").submit(function(event) {
+    var $input = $(event.target).find("input");
+    var comment = $input.val();
+    
+    if(comment != "") {
+      var html = $("<li>").text(comment);
+      html.prependTo("#comments");
+      $input.val("");
+    }
+    return false;
   });
-  
-  $().click(function() {
-    $(this).css('border-color', 'blue');
-  });
-  
-});
+}
+$(document).ready(main);
