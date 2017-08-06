ff78a2eb42dc199dc10c7c2d2fdcdd48c1d0f018
avarona <avarona10@gmail.com>
2017-08-06 15:09:01 -0400

    feat(footer): add footer

---
+++ app/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="copyright">
+        Copyright © {2017}, Alejandro Varona, All rights reserved
+      </div>
+    </div>
+  )
+}
+
+export default Footer;
--- app/containers/AppContainer.jsx
+++ app/containers/AppContainer.jsx
@@ -4,6 +4,7 @@ import Header from '../components/Header.jsx';
 import About from '../components/About.jsx';
 import Portfolio from '../components/Portfolio.jsx';
 import Contact from '../components/Contact.jsx';
+import Footer from '../components/Footer.jsx';
 
 const AppContainer = () =>  {
 		return (
@@ -12,6 +13,7 @@ const AppContainer = () =>  {
 				<About />
 				<Portfolio />
 				<Contact />
+				<Footer />
       </div>
     )
   }
