c86184f1bf5ad544d5c22b2cd88f65980e36fd9c
Alejandro Varona <avarona10@gmail.com>
2019-09-30 17:30:12 -0400

    edit eslint/prettier config

--- v2/.eslintrc.js
+++ v2/.eslintrc.js
@@ -3,9 +3,6 @@ module.exports = {
   extends: [
     'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-    'plugin:import/typescript',
   ],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
@@ -16,12 +13,9 @@ module.exports = {
   },
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. '@typescript-eslint/explicit-function-return-type': "off",
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/prefer-interface': 'off',
     '@typescript-eslint/no-object-literal-type-assertion': 'off',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
   },
   settings: {
     react: {
