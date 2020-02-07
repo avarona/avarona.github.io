6e95199f2682287bf9bd0cf3d6bc21c880fa54b1
Alejandro Varona <avarona10@gmail.com>
2020-02-06 21:13:53 -0500

    make navbar sticky

--- v2/src/components/NavBar/index.jsx
+++ v2/src/components/NavBar/index.jsx
@@ -3,35 +3,31 @@ import styles from "./styles.module.scss";
 import headshot from "../../images/headshot.png";
 import { Icon } from "@avarona/react-components";
 
-function NavBar() {
-  return (
-    <nav className={styles.navContainer}>
-      <div className={styles.wrapper}>
-        <Icon addClass={styles.image} img={headshot} alt='headshot' />
+const NavBar = () => (
+  <nav className={styles.navContainer}>
+    <Icon addClass={styles.image} img={headshot} alt='headshot' size={40} />
 
-        <ul className={styles.navList}>
-          <li className={styles.navItem}>
-            <a href='#portfolio'>Projects</a>
-          </li>
-          <li className={styles.navItem}>
-            <a href='#about'>About</a>
-          </li>
-          <li className={styles.navItem}>
-            <a href='#github'>Github</a>
-          </li>
-          <li className={styles.navItem}>
-            <a href='#linkedin'>LinkedIn</a>
-          </li>
-          <li className={styles.navItem}>
-            <a href='#angel'>Angel.co</a>
-          </li>
-          <li className={styles.navItem}>
-            <a href='#contact'>Contact</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
-}
+    <ul className={styles.navList}>
+      <li className={styles.navItem}>
+        <a href='#portfolio'>Projects</a>
+      </li>
+      <li className={styles.navItem}>
+        <a href='#about'>About</a>
+      </li>
+      <li className={styles.navItem}>
+        <a href='#github'>Github</a>
+      </li>
+      <li className={styles.navItem}>
+        <a href='#linkedin'>LinkedIn</a>
+      </li>
+      <li className={styles.navItem}>
+        <a href='#angel'>Angel.co</a>
+      </li>
+      <li className={styles.navItem}>
+        <a href='#contact'>Contact</a>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default NavBar;
