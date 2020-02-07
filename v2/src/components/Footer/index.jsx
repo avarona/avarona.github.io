25d6b01a80b5057b59017aae16b36df50b6cddf2
Alejandro Varona <avarona10@gmail.com>
2020-02-07 14:00:40 -0500

    add Footer

---
+++ v2/src/components/Footer/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+const Footer = () => (
+  <footer className={styles.footerContainer}>Copyright varona.io</footer>
+);
+
+export default Footer;
--- v2/src/containers/App/index.jsx
+++ v2/src/containers/App/index.jsx
@@ -4,6 +4,7 @@ import Projects from "../Projects";
 import About from "../About";
 import Contact from "../Contact";
 import NavBar from "../../components/NavBar";
+import Footer from "../../components/Footer";
 import styles from "./styles.module.scss";
 
 const App = () => (
@@ -16,6 +17,7 @@ const App = () => (
         <Route path='/contact' component={Contact} />
         <Route exact path='/' />
       </div>
+      <Footer />
     </Router>
   </div>
 );
