a89f8475ba73c1668619c9a29e85b2417fed079f
Alejandro Varona <avarona10@gmail.com>
2020-02-10 17:27:04 -0500

    use grommet

--- v2/src/components/Footer/index.jsx
+++ v2/src/components/Footer/index.jsx
@@ -1,8 +1,32 @@
 import React from "react";
-import styles from "./styles.module.scss";
+import { Footer as GrommetFooter, Text, Box, Anchor } from "grommet";
+import { Github, LinkedinOption } from "grommet-icons";
 
 const Footer = () => (
-  <footer className={styles.footerContainer}>Copyright varona.io</footer>
+  <GrommetFooter background='light-2' pad='small'>
+    <Box align='center' direction='row' gap='xsmall'>
+      <Text alignSelf='center' color='brand' size='small'>
+        Alejandro Varona
+      </Text>
+    </Box>
+
+    <Box direction='row' gap='xxsmall' justify='center'>
+      <Anchor
+        a11yTitle='Share feedback on Github'
+        href='https://www.github.com/avarona'
+        icon={<Github color='brand' />}
+      />
+      <Anchor
+        a11yTitle='Check out my LinkedIn'
+        href='https://www.linkedin.com/in/alejandro-varona'
+        icon={<LinkedinOption color='brand' />}
+      />
+    </Box>
+
+    <Text textAlign='center' size='xsmall'>
+      Copyright © <Anchor href='https://www.varona.io'>varona.io</Anchor>
+    </Text>
+  </GrommetFooter>
 );
 
 export default Footer;
