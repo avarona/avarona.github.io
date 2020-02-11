0d513a27463593ca84ab7d7e217744401e233c85
Alejandro Varona <avarona10@gmail.com>
2020-02-11 16:43:42 -0500

    page layout

--- v2/src/containers/App/index.jsx
+++ v2/src/containers/App/index.jsx
@@ -1,25 +1,35 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Grommet, Main, Box } from "grommet";
 import Projects from "../Projects";
 import About from "../About";
 import Contact from "../Contact";
 import NavBar from "../../components/NavBar";
 import Footer from "../../components/Footer";
-import styles from "./styles.module.scss";
+
+const theme = {
+  global: {
+    font: {
+      family: "Arial, Helvetica, sans-serif"
+    }
+  }
+};
 
 const App = () => (
-  <div className={styles.container}>
-    <Router>
-      <NavBar />
-      <div className={styles.content}>
-        <Route path='/projects' component={Projects} />
-        <Route path='/about' component={About} />
-        <Route path='/contact' component={Contact} />
-        <Route exact path='/' />
-      </div>
-      <Footer />
-    </Router>
-  </div>
+  <Router>
+    <Grommet theme={theme} full>
+      <Main>
+        <NavBar />
+        <Box flex>
+          <Route path='/projects' component={Projects} />
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+          <Route exact path='/' component={Projects} />
+        </Box>
+        <Footer />
+      </Main>
+    </Grommet>
+  </Router>
 );
 
 export default App;
